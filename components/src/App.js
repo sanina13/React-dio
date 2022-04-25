27201e4c7c7b95aea1a6eaa9cb9216b90f0024e2
sanina13 <saninatiago11@gmail.com>
2022-04-25 15:29:22 +0100

    components update

--- components/src/App.js
+++ components/src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from "./Button"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+	const soma = (n1, n2) => n1 + n2
+	return(
+		<div className="App">
+			Hello World
+			{soma(2,3)}
+		</div>
+	)	
 }
 
 export default App;
