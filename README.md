b2cf48a9e37e48fa86da2686c1e945c72066877c
sanina13 <saninatiago11@gmail.com>
2022-04-25 15:15:28 +0100

    render e components

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # React-dio
  react bootcamp carrefour dio
+
+Create React app
+1º - npx create-react-app nameapp
+2º - entrar na app cd nameapp
+3º - npm start
---
+++ renderization/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+const element = "Digital Inovation"
+const element2 = <h1>Olá turma =D</h1>
+
+function App() {
+  return (
+    <div className="App">
+		{element}
+		{element2}
+    </div>
+  );
+}
+
+export default App;
